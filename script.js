186943a59fbd9f89c2ede01e53bf1cd2446e21be
kqwang123 <kqwang123@gmail.com>
2023-07-26 19:09:54 -0600

    detects new posts

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+require('dotenv').config();
 
 async function run() {
     const browser = await puppeteer.launch({headless : false}); 
@@ -14,35 +15,33 @@ async function run() {
 
     await page.click('.sign-in-form__submit-btn--full-width');
     await new Promise((resolve) => setTimeout(resolve, 8000));
-    
-    // Send a message to Tim
 
-    // await page.click(".msg-conversation-listitem__participant-names.msg-conversation-card__participant-names");
-    // await new Promise((resolve) => setTimeout(resolve, 2000));
-    // await page.click(".msg-form__contenteditable.t-14.t-black--light.t-normal.flex-grow-1.full-height.notranslate");
-    // await new Promise((resolve) => setTimeout(resolve, 2000));
-    // await page.type(".msg-form__contenteditable.t-14.t-black--light.t-normal.flex-grow-1.full-height.notranslate", "Hello, I am a bot. I am sending this message to you because I am testing my puppeteer script. Please ignore this message. Do not be afraid.");
-    // await new Promise((resolve) => setTimeout(resolve, 2000));
-    // await page.click("button.msg-form__send-button");
-    // await new Promise((resolve) => setTimeout(resolve, 2000));
+    while (true) {
+        // Comment on Tim's post
+        
+        await page.goto("https://www.linkedin.com/in/tim-cai-a9b285221/recent-activity/all/");
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        
+        var likeButtonSelector = ".artdeco-button.artdeco-button--muted.artdeco-button--4.artdeco-button--tertiary.ember-view.social-actions-button.react-button__trigger";
+        var ariaLabel = await page.$eval(likeButtonSelector, (element) => element.getAttribute("aria-label"));
 
-    // Comment on Tim's post
+        if (ariaLabel == "React Like to Tim Cai’s post") {
+            
+            await page.click(likeButtonSelector);
+            await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    await page.goto("https://www.linkedin.com/in/tim-cai-a9b285221/recent-activity/all/");
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    await page.click(".artdeco-button.artdeco-button--muted.artdeco-button--4.artdeco-button--tertiary.ember-view.social-actions-button.react-button__trigger");
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    var quote = await page.$eval(".update-components-text.relative.feed-shared-update-v2__commentary", text => text.innerText);
-    
-    await page.type(".ql-editor.ql-blank", "Thanks Tim! This has motivated me as well to " + quote.trim().substring(5, quote.length));
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    await page.click(".comments-comment-box__submit-button.mt3.artdeco-button.artdeco-button--1.artdeco-button--primary.ember-view");
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+            var quote = await page.$eval(".update-components-text.relative.feed-shared-update-v2__commentary", text => text.innerText);
+            
+            await page.type(".ql-editor.ql-blank", "Thanks Tim! This has motivated me as well to " + quote.trim().substring(5, quote.length));
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+            await page.click(".comments-comment-box__submit-button.mt3.artdeco-button.artdeco-button--1.artdeco-button--primary.ember-view");
+            await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    await page.screenshot({path: 'linkedin.png'});
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    await browser.close();
+            await page.screenshot({path: 'linkedin.png'});
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+        }
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+    }
 }
 
 run();
